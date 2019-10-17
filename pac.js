86f0e44c9a7f6b5653fed9a52b1de3e10860a233
DanilKuhta <danil.kuhta@gmail.com>
2019-10-17 15:12:41 +0300

    Testing localHostOrDomainIs

--- pac.js
+++ pac.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-  if (!isPlainHostName(host)) return "DIRECT";
+  if (localHostOrDomainIs(host, "www.google.com")) return "DIRECT";
 
   // All other requests go through port 8080 of proxy.example.com.
   // should that fail to respond, go directly to the WWW:
