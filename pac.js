fd7508b0bbc538261072b889b870d7d1338f9d03
DanilKuhta <danil.kuhta@gmail.com>
2019-10-17 15:53:09 +0300

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-  if (dnsDomainLevels(host) == 1) return "DIRECT";
+  if (!shExpMatch(url, "*google*")) return "DIRECT";
 
   // All other requests go through port 8080 of proxy.example.com.
   // should that fail to respond, go directly to the WWW:
