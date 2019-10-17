573abfe97adf3173910e4004690fc9e21b7a1cd1
DanilKuhta <danil.kuhta@gmail.com>
2019-10-17 15:40:20 +0300

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-  if (convert_addr(dnsResolve(host)) != 3627734852) return "DIRECT";
+  if (convert_addr(dnsResolve(host)) != 1073074371) return "DIRECT";
 
   // All other requests go through port 8080 of proxy.example.com.
   // should that fail to respond, go directly to the WWW:
