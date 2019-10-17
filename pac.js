f8a19d1cb80fbf74f250fe901180b8d587e1db9d
DanilKuhta <danil.kuhta@gmail.com>
2019-10-17 15:20:06 +0300

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-  if (isResolvable(host)) return "DIRECT";
+  if (isInNet(host, "216.58.207.46", 255)) return "DIRECT";
 
   // All other requests go through port 8080 of proxy.example.com.
   // should that fail to respond, go directly to the WWW:
