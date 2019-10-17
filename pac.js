953639091dc0bef0e8feb5c5d73a1195c39bf5b7
DanilKuhta <danil.kuhta@gmail.com>
2019-10-17 15:16:30 +0300

    Testing isResolvable

--- pac.js
+++ pac.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-  if (localHostOrDomainIs(host, "www.google.com")) return "DIRECT";
+  if (isResolvable(host)) return "DIRECT";
 
   // All other requests go through port 8080 of proxy.example.com.
   // should that fail to respond, go directly to the WWW:
