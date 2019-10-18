3fd1fa56a18959255a3367177ad409f994ccee0d
DanilKuhta <danil.kuhta@gmail.com>
2019-10-18 16:36:16 +0300

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-  if (weekdayRange("MON", "FRI")) return "DIRECT";
+  if (!weekdayRange("MON", "FRI")) return "DIRECT";
 
   // All other requests go through port 8080 of proxy.example.com.
   // should that fail to respond, go directly to the WWW:
